198f350c36ae0b7638661335096be2b788c9f57c
D_N_C_2004 <dinisncarraca2@gmail.com>
2025-12-28 16:29:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 ## Onde se vai escolher que algoritmo utilizar
+from bfs_dfs import dfs, bfs
+
+#matriz_jogo_quinze = [[2,5,6,8],[1,4,9,10],[12,14,15,3], [13,7,11,0]] # Matriz à séria
+matriz_jogo_quinze = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,0,15]] # Matriz de teste
+matriz_goal = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,15,0]]
