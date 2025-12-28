4f03ee099a54eb0032b06c9fbd3a17294dec98fa
D_N_C_2004 <dinisncarraca2@gmail.com>
2025-12-28 18:27:19 +0000

    Update supervisionadas.py

--- supervisionadas.py
+++ supervisionadas.py
@@ -1,9 +1,18 @@
 import heapq
 from comuns import matriz_tuplo, get_neighbors
+# Impossível
+# matriz_jogo_quinze = [[2,5,6,8],[1,4,9,10],[12,14,15,3], [13,7,11,0]]
+# matriz_jogo_quinze = [[14,13,15,7],[11,12,9,5],[6,0,2,1],[4,8,10,3]]
 
-#matriz_jogo_quinze = [[2,5,6,8],[1,4,9,10],[12,14,15,3], [13,7,11,0]]
-matriz_jogo_quinze = [[1,2,3,4], [5,6,7,8], [9,14,10,12], [0,13,11,15]]
-#matriz_jogo_quinze = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,0,15]]
+# Dificeis
+#matriz_jogo_quinze = [[5, 0, 2, 11],[14, 1, 3, 6],[9, 8, 13, 7],[10, 15, 4, 12]]
+
+# Médias
+matriz_jogo_quinze = [[1,2,3,4],[5,6,0,8],[9,10,7,11],[13,14,15,12]]
+
+# Fáceis
+# matriz_jogo_quinze = [[1,2,3,4], [5,6,7,8], [9,14,10,12], [0,13,11,15]]
+# matriz_jogo_quinze = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,0,15]]
 
 matriz_goal = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,15,0]]
 
@@ -45,6 +54,10 @@ def gbfs(matriz_tabuleiro, matriz_goal):
     while fila:
         _, node = heapq.heappop(fila)
         contador += 1
+        # print(f"DEBUG -- Nó atual: {node}")
+        
+        if contador % 10000 == 0:
+            print(f"Tentativa: {contador}")
         
         if node in visitados:
             continue
@@ -52,7 +65,7 @@ def gbfs(matriz_tabuleiro, matriz_goal):
         visitados.add(node)
         
         if node == goal:
-            print(f"GBFS resolveu o tabuleiro em {contador} tentativas.")
+            print(f"O || GBFS || resolveu o tabuleiro em {contador} tentativas.")
             return True
         
         for vizi in get_neighbors(node):
@@ -77,13 +90,16 @@ def unc(matriz_tabuleiro, matriz_goal):
         contador += 1
         #print(f"DEBUG -- Custo atual: {custo} | Nó atual: {node}")
         
+        if contador % 10000 == 0:
+            print(f"Tentativa: {contador}")
+        
         if node in visitados and visitados[node] <= custo:
             continue
         
         visitados[node] = custo
         
         if node == goal:
-            print(f"O UCS resolveu o tabuleiro em {contador} tentativas com um custo total de {custo}.")
+            print(f"O || UCS || resolveu o tabuleiro em {contador} tentativas com um custo total de {custo}.")
             return True
         
         for vizi in get_neighbors(node):
@@ -102,18 +118,24 @@ def astar(matriz_tabuleiro, matriz_goal):
     visitados = {}
     contador = 0
     
+    custos_menores = {tabuleiro: 0}
+    
     while fila:
         _,custo_atual, node = heapq.heappop(fila)
         contador += 1
         # print(f"DEBUG -- Custo atual: {custo_atual} | Nó atual: {node}")
+        
+        if contador % 10000 == 0:
+            print(f"Tentativa: {contador}")
         
         if node in visitados and visitados[node] <= custo_atual:
             continue
         
         visitados[node] = custo_atual
         
+        
         if node == goal:
-            print(f"O A* resolveu o tabuleiro em {contador} tentativas com um custo total de {custo_atual}.")
+            print(f"O || A* || resolveu o tabuleiro em {contador} tentativas com um custo total de {custo_atual}.")
             return True
         
         for vizi in get_neighbors(node):
@@ -127,7 +149,5 @@ if __name__ == "__main__":
     # dist_manhatan(matriz_jogo_quinze, matriz_goal)
 
     gbfs(matriz_jogo_quinze, matriz_goal)
-    
     unc(matriz_jogo_quinze, matriz_goal)
-    
     astar(matriz_jogo_quinze, matriz_goal)
