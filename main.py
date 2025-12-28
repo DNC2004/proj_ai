09260f5a0102887403a0964b7d34872daa0dca79
D_N_C_2004 <dinisncarraca2@gmail.com>
2025-12-28 18:26:50 +0000

    Mais Matrizes para testar

--- main.py
+++ main.py
@@ -2,6 +2,18 @@
 from n_supervisonadas import dfs, bfs
 from supervisionadas import unc, astar
 
-#matriz_jogo_quinze = [[2,5,6,8],[1,4,9,10],[12,14,15,3], [13,7,11,0]] # Matriz à séria
-matriz_jogo_quinze = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,0,15]] # Matriz de teste
+# Impossível
+# matriz_jogo_quinze = [[2,5,6,8],[1,4,9,10],[12,14,15,3], [13,7,11,0]]
+# matriz_jogo_quinze = [[14,13,15,7],[11,12,9,5],[6,0,2,1],[4,8,10,3]]
+
+# Dificeis
+#matriz_jogo_quinze = [[5, 0, 2, 11],[14, 1, 3, 6],[9, 8, 13, 7],[10, 15, 4, 12]]
+
+# Médias
+#matriz_jogo_quinze = [[1,2,3,4],[5,6,0,8],[9,10,7,11],[13,14,15,12]]
+
+# Fáceis
+# matriz_jogo_quinze = [[1,2,3,4], [5,6,7,8], [9,14,10,12], [0,13,11,15]]
+# matriz_jogo_quinze = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,0,15]]
+
 matriz_goal = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,15,0]]
