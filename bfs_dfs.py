5065f3d6d44ea35ca13d0371656eb17a4337b6c5
D_N_C_2004 <dinisncarraca2@gmail.com>
2025-12-28 17:40:31 +0000

    Update bfs_dfs.py

--- bfs_dfs.py
+++ bfs_dfs.py
@@ -1,29 +1,10 @@
-# Funções para os algoritmos BFS e DFS
+from comuns import matriz_tuplo, get_neighbors
 
+# Funções para os algoritmos BFS e DFS
 #matriz_jogo_quinze = [[2,5,6,8],[1,4,9,10],[12,14,15,3], [13,7,11,0]]
 matriz_jogo_quinze = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,0,15]]
 matriz_goal = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,15,0]]
 
-
-def matriz_tuplo(matriz):
-    return tuple(num for row in matriz for num in row)
-
-def get_neighbors(state):
-    neighbors = []
-    zero = state.index(0)
-    row, col = divmod(zero, 4)
-
-    moves = [(-1,0), (1,0), (0,-1), (0,1)]
-
-    for dr, dc in moves:
-        nr, nc = row + dr, col + dc
-        if 0 <= nr < 4 and 0 <= nc < 4:
-            new_zero = nr * 4 + nc
-            new_state = list(state)
-            new_state[zero], new_state[new_zero] = new_state[new_zero], new_state[zero]
-            neighbors.append(tuple(new_state))
-
-    return neighbors
 
 
 # Modificar para funcionar com as matrizes
