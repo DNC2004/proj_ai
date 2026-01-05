dabdca77b9a01f1c752f5199fc29d34dc23e19d7
D_N_C_2004 <dinisncarraca2@gmail.com>
2026-01-05 19:29:07 +0000

    Colocar Goal global

--- supervisionadas.py
+++ supervisionadas.py
@@ -1,27 +1,24 @@
 import heapq
-from comuns import matriz_tuplo, get_neighbors, dist_manhatan
+from comuns import matriz_tuplo, get_neighbors, dist_manhatan, GOAL
 # Impossível
 # matriz_jogo_quinze = [[2,5,6,8],[1,4,9,10],[12,14,15,3], [13,7,11,0]]
 # matriz_jogo_quinze = [[14,13,15,7],[11,12,9,5],[6,0,2,1],[4,8,10,3]]
 
 # Dificeis
-#matriz_jogo_quinze = [[5, 0, 2, 11],[14, 1, 3, 6],[9, 8, 13, 7],[10, 15, 4, 12]]
+matriz_jogo_quinze = [[5, 0, 2, 11],[14, 1, 3, 6],[9, 8, 13, 7],[10, 15, 4, 12]]
 
 # Médias
-matriz_jogo_quinze = [[1,2,3,4],[5,6,0,8],[9,10,7,11],[13,14,15,12]]
+# matriz_jogo_quinze = [[1,2,3,4],[5,6,0,8],[9,10,7,11],[13,14,15,12]]
 
 # Fáceis
 # matriz_jogo_quinze = [[1,2,3,4], [5,6,7,8], [9,14,10,12], [0,13,11,15]]
 # matriz_jogo_quinze = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,0,15]]
 
-matriz_goal = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,15,0]]
-
-
 # Greedy Best First Search (Não contabiliza o custo só a distância)
 # Limite = 0 --> Não queremos limite
-def gbfs(matriz_tabuleiro, matriz_goal, limite):
+def gbfs(matriz_tabuleiro, limite):
     tabuleiro = matriz_tuplo(matriz_tabuleiro)
-    goal = matriz_tuplo(matriz_goal)
+    goal = GOAL
     
     fila = []
     heapq.heappush(fila, (dist_manhatan(tabuleiro, goal), tabuleiro))
@@ -57,10 +54,10 @@ def gbfs(matriz_tabuleiro, matriz_goal, limite):
     return False
 
 # Uniforme Cost Search (Custo sem heurística)
-def unc(matriz_tabuleiro, matriz_goal,limite):
+def unc(matriz_tabuleiro,limite):
     fila = []
     tabuleiro = matriz_tuplo(matriz_tabuleiro)
-    goal = matriz_tuplo(matriz_goal)
+    goal = GOAL
     
     heapq.heappush(fila, (0, tabuleiro))
     
@@ -94,10 +91,10 @@ def unc(matriz_tabuleiro, matriz_goal,limite):
     return False
 
 # A* (Custo com heurística)
-def astar(matriz_tabuleiro, matriz_goal,limite):
+def astar(matriz_tabuleiro,limite):
     fila = []
     tabuleiro = matriz_tuplo(matriz_tabuleiro)
-    goal = matriz_tuplo(matriz_goal)
+    goal = GOAL
     
     heapq.heappush(fila, (dist_manhatan(tabuleiro, goal),0,tabuleiro))
     
