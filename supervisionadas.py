d267c2cbc5eaf25bf058c366eb02d107b52aa311
D_N_C_2004 <dinisncarraca2@gmail.com>
2026-01-05 19:11:34 +0000

    Update supervisionadas.py

--- supervisionadas.py
+++ supervisionadas.py
@@ -1,5 +1,5 @@
 import heapq
-from comuns import matriz_tuplo, get_neighbors
+from comuns import matriz_tuplo, get_neighbors, dist_manhatan
 # Impossível
 # matriz_jogo_quinze = [[2,5,6,8],[1,4,9,10],[12,14,15,3], [13,7,11,0]]
 # matriz_jogo_quinze = [[14,13,15,7],[11,12,9,5],[6,0,2,1],[4,8,10,3]]
@@ -16,28 +16,6 @@ matriz_jogo_quinze = [[1,2,3,4],[5,6,0,8],[9,10,7,11],[13,14,15,12]]
 
 matriz_goal = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,15,0]]
 
-
-
-def dist_manhatan(tabuleiro, goal): # Distância da posição atual do quadrado para a posição onde este é suposto estar
-    dist = 0
-    
-    for i, quadrado in enumerate(tabuleiro):
-        if quadrado == 0:
-            continue
-        
-        fila, coluna = divmod(i,4) # Posição do quadrado atual
-        
-        goal_fila, goal_coluna = divmod(goal.index(quadrado), 4) # Posição suposta do quadrado
-
-        dist += abs(fila - goal_fila) + abs(coluna - goal_coluna) # Calcular a distância em si
-
-        # DEBUG        
-        # print(f"DEBUG -- O quadrado {quadrado} está na posição: x = {fila} | y = {coluna}") 
-        # print(f"DEBUG -- O objetivo está na posição: x = {goal_fila} | y = {goal_coluna}") 
-        # print(f"DEBUG -- Distância entre o quadrado {goal_fila} e {goal_coluna}: {dist}") 
-    
-    return dist
-        
 
 # Greedy Best First Search (Não contabiliza o custo só a distância)
 # Limite = 0 --> Não queremos limite
