7b7e19fbea13a686c2c0a09c6805bb7d88261681
D_N_C_2004 <dinisncarraca2@gmail.com>
2026-01-05 19:27:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 ## Onde se vai escolher que algoritmo utilizar
 from n_supervisonadas import dfs, bfs
 from supervisionadas import unc, astar,gbfs
+from ref_learning import r_learning
+
+# matriz_goal = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,15,0]] Não é preciso
 
 # Impossível
 # matriz_jogo_quinze = [[2,5,6,8],[1,4,9,10],[12,14,15,3], [13,7,11,0]]
@@ -10,10 +13,18 @@ from supervisionadas import unc, astar,gbfs
 #matriz_jogo_quinze = [[5, 0, 2, 11],[14, 1, 3, 6],[9, 8, 13, 7],[10, 15, 4, 12]]
 
 # Médias
-#matriz_jogo_quinze = [[1,2,3,4],[5,6,0,8],[9,10,7,11],[13,14,15,12]]
+matriz_jogo_quinze = [[1,2,3,4],[5,6,0,8],[9,10,7,11],[13,14,15,12]]
 
 # Fáceis
 # matriz_jogo_quinze = [[1,2,3,4], [5,6,7,8], [9,14,10,12], [0,13,11,15]]
 # matriz_jogo_quinze = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,0,15]]
 
-matriz_goal = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,15,0]]
+
+#dfs(matriz_jogo_quinze)
+#bfs(matriz_jogo_quinze)
+
+unc(matriz_jogo_quinze, 1000)
+astar(matriz_jogo_quinze, 1000)
+gbfs(matriz_jogo_quinze, 1000)
+
+r_learning(matriz_jogo_quinze)
