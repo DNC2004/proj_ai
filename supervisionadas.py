8ca4d852a3d09ead3a60486d42428290ed92b134
D_N_C_2004 <dinisncarraca2@gmail.com>
2026-01-10 21:46:55 +0000

    Retirar dispensáveis

--- supervisionadas.py
+++ supervisionadas.py
@@ -1,19 +1,5 @@
-# Não funciona com os goals
 import heapq
 from comuns import matriz_tuplo, get_neighbors, dist_manhatan, GOALS, MAX_LIMIT
-# Impossível
-# matriz_jogo_quinze = [[2,5,6,8],[1,4,9,10],[12,14,15,3], [13,7,11,0]]
-# matriz_jogo_quinze = [[14,13,15,7],[11,12,9,5],[6,0,2,1],[4,8,10,3]]
-
-# Dificeis
-matriz_jogo_quinze = [[5, 0, 2, 11],[14, 1, 3, 6],[9, 8, 13, 7],[10, 15, 4, 12]]
-
-# Médias
-# matriz_jogo_quinze = [[1,2,3,4],[5,6,0,8],[9,10,7,11],[13,14,15,12]]
-
-# Fáceis
-# matriz_jogo_quinze = [[1,2,3,4], [5,6,7,8], [9,14,10,12], [0,13,11,15]]
-# matriz_jogo_quinze = [[1,2,3,4],[5,6,7,8],[9,10,11,12], [13,14,0,15]]
 # Limite = -1 --> Não queremos limite
 
 # Greedy Best First Search (Não contabiliza o custo só a distância)
@@ -144,12 +130,5 @@ def astar(matriz_tabuleiro,limite, tipo_goal):
             custo_att = custo_atual + 1
             custo_estimado_att = custo_att + dist_manhatan(vizi,tipo_goal)
             heapq.heappush(fila, (custo_estimado_att, custo_att, vizi)) 
+            
     return False
-
-
-if __name__ == "__main__":
-    # dist_manhatan(matriz_jogo_quinze,
-
-    #gbfs(matriz_jogo_quinze,0)
-    #unc(matriz_jogo_quinze,0)
-    astar(matriz_jogo_quinze,-1)
